58cb153e4dfa868d816859f0ffb6025bec73efcd
Gustavo Fragata Rousseau <gustavo.fragata@gmail.com>
2025-01-09 01:56:07 -0300

    Upload Novas aulas
    Aulas Novas

---
+++ aula7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Gustavo Fragata Rousseau'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois) # Gustavo Fragata Rousseau em seguida separado por virgula 4
+int_um = int('1') # isso pode ser feito para facilitar 
+print(int_um, type(int_um)) # 1 <class 'int'>
+
+nome= 'Gustavo'
+idade = 20
+maior_de_idade = idade >= 18 
+print('Nome:', nome, 'Idade:', idade)
+print('Maior de idade?', maior_de_idade) # Nome: Gustavo Idade: 20 Maior de idade: True
